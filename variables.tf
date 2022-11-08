7f1d807108563b42248e798e58dd3dc4ca0a1f5e
Derry Bradley <derrybradley@live.ie>
2022-11-08 15:20:10 +0100

    fmt

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
- variable "config_path" {
-    description = "path to kubeconfig file"
-    type = string
-    default = "~/.kube/config"
- }
+variable "config_path" {
+  description = "path to kubeconfig file"
+  type        = string
+  default     = "~/.kube/config"
+}
 
-  variable "namespace" {
-    description = "namespace to deploy container runner to"
-    type = string
-    default = "default"
- }
+variable "namespace" {
+  description = "namespace to deploy container runner to"
+  type        = string
+  default     = "default"
+}
