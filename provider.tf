8a56fdc7daae81377292b3dabfc0e10f6d960ec6
Derry Bradley <derrybradley@live.ie>
2022-11-08 11:28:15 +0100

    seperate tf files

--- provider.tf
+++ provider.tf
@@ -3,26 +3,3 @@ provider "helm" {
     config_path = "~/.kube/config"
   }
 }
-
-resource "helm_release" "container-agent" {
-
-  name = "container-agent"
-
-  repository = "https://packagecloud.io/circleci/container-agent/helm"
-  chart      = "container-agent"
-  namespace  = "default"
-
-  values = [
-    "${file("values.yaml")}"
-  ]
-
-  set {
-    name  = "replicaCount"
-    value = 2
-  }
-
-  set {
-    name  = "rbac.clusterReadOnlyRole"
-    value = "true"
-  }
-}
